44c90dc7d26c702172aab12ef0bdd6910db67d65
Alves-ctrl <g.alves1608@gmail.com>
2026-09-19 23:51:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
-from Treinador import Treinador
-from Duelo import Duelo
+from visao.Visao import Visao
 
 
 def main():
-
-    print(" BEM-VINDO AO JOGO POKEMO")
-
-    nome1= input("Teinador 01\n")
-    nome2= input("teinador 02\n")
-    treinador1 = Treinador(1, nome1)
-    treinador2 = Treinador(2, nome2)
-
-
-    duelo = Duelo(1, treinador1, treinador2)
-
-    
-    duelo.times()
-
-    duelo.iniciar()
-
-    print(f"\nVencedor: {duelo.vencedor.nome}")
+    visao = Visao()
+    visao.iniciar()
 
 
 if __name__ == "__main__":
